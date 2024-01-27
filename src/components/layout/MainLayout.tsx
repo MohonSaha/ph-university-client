b33456002e92876182c14b33d81847faf02fcbac
MohonSaha <practicewithmohon@gmail.com>
2024-01-27 09:10:33 +0600

    implement redux slice

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,5 +1,5 @@
 import { Layout, Menu, MenuProps } from "antd";
-const { Header, Content, Footer, Sider } = Layout;
+const { Header, Content } = Layout;
 import {
   UploadOutlined,
   UserOutlined,
@@ -7,42 +7,12 @@ import {
 } from "@ant-design/icons";
 import { createElement } from "react";
 import { Outlet } from "react-router-dom";
-import { adminSidebarItems } from "../../routes/admin.routes";
+import Sidebar from "./Sidebar";
 
 const MainLayout = () => {
   return (
     <Layout style={{ height: "100vh" }}>
-      <Sider
-        breakpoint="lg"
-        collapsedWidth="0"
-        onBreakpoint={(broken) => {
-          console.log(broken);
-        }}
-        onCollapse={(collapsed, type) => {
-          console.log(collapsed, type);
-        }}
-      >
-        <div
-          style={{
-            color: "white",
-            fontWeight: "900",
-            textAlign: "center",
-            height: "4rem",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            fontSize: "20px",
-          }}
-        >
-          PH University
-        </div>
-        <Menu
-          theme="dark"
-          mode="inline"
-          defaultSelectedKeys={["4"]}
-          items={adminSidebarItems}
-        />
-      </Sider>
+      <Sidebar />
       <Layout>
         <Header style={{ padding: 0 }} />
         <Content style={{ margin: "24px 16px 0" }}>
@@ -55,9 +25,6 @@ const MainLayout = () => {
             <Outlet />
           </div>
         </Content>
-        <Footer style={{ textAlign: "center" }}>
-          Ant Design ©{new Date().getFullYear()} Created by Ant UED
-        </Footer>
       </Layout>
     </Layout>
   );
---
+++ src/components/layout/Sidebar.tsx
@@ -0,0 +1,60 @@
+import { Layout, Menu } from "antd";
+import { sidebarItemsGenerator } from "../../utils/sidebarItemsGenerator";
+import { adminPaths } from "../../routes/admin.routes";
+import { facultyPaths } from "../../routes/faculty.routes";
+import { studentPaths } from "../../routes/students.routes";
+
+const { Sider } = Layout;
+
+const userRole = {
+  ADMIN: "admin",
+  FACULTY: "faculty",
+  STUDNET: "student",
+};
+
+const Sidebar = () => {
+  const role = "student";
+  let sidebarItems;
+
+  switch (role) {
+    case userRole.ADMIN:
+      sidebarItems = sidebarItemsGenerator(adminPaths, userRole.ADMIN);
+      break;
+    case userRole.FACULTY:
+      sidebarItems = sidebarItemsGenerator(facultyPaths, userRole.FACULTY);
+      break;
+    case userRole.STUDNET:
+      sidebarItems = sidebarItemsGenerator(studentPaths, userRole.STUDNET);
+      break;
+
+    default:
+      break;
+  }
+
+  return (
+    <Sider breakpoint="lg" collapsedWidth="0">
+      <div
+        style={{
+          color: "white",
+          fontWeight: "900",
+          textAlign: "center",
+          height: "4rem",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          fontSize: "20px",
+        }}
+      >
+        PH University
+      </div>
+      <Menu
+        theme="dark"
+        mode="inline"
+        defaultSelectedKeys={["4"]}
+        items={sidebarItems}
+      />
+    </Sider>
+  );
+};
+
+export default Sidebar;
